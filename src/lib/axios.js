dac12211fb06c02910e8f6ead159868e5301b1b0
abdelhalim <abdelhalim@gmail.com>
2025-11-11 00:55:39 +0200

    asdjl;f;ajskdf

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "https://backoo-production.up.railway.app",
+  baseURL: "https://backoo-production.up.railway.app",
   withCredentials: true,
 });
